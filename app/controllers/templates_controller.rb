b5bbe53f0004695c6895e8be7310d57d0cce33fb
flower-pot <flowerpot@pondati.net>
2014-03-18 22:03:42 +0100

    remove resource, resource_class, and resource_name helper methods

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -1,32 +1,14 @@
 class TemplatesController < ApplicationController
   layout false
-  helper_method :resource_name, :resource_class, :resource, :devise_mapping
+  helper_method :devise_mapping
 
   before_filter lambda{ puts "Logged in? -> #{user_signed_in?}" }
-
-  def sign_up
-    @resource_name = :user
-    @resource = User.new
-    @resource_class = @resource.class
-  end
 
   def template
     render template: 'templates/' + params[:path], layout: false
   end
 
   private
-
-  def resource_name
-    @resource_name
-  end
-
-  def resource_class
-    @resource_class
-  end
-
-  def resource
-    @resource
-  end
 
   def devise_mapping
     @devise_mapping ||= Devise.mappings[:user]
