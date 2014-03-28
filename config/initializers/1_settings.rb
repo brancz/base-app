a195656aaa6dd4a483c0dc221935098f81127071
flower-pot <flowerpot@pondati.net>
2014-03-28 14:01:44 +0100

    add redis url setting
    if not set then heroku's RAILSTOGO environment variable is used
    in dev and test environment the default redis server is used

--- config/initializers/1_settings.rb
+++ config/initializers/1_settings.rb
@@ -2,3 +2,6 @@ class Settings < Settingslogic
   source "#{Rails.root}/config/config.yml"
   namespace Rails.env
 end
+
+Settings['redis'] ||= Settingslogic.new({})
+Settings.redis['url'] ||= ENV["REDISTOGO_URL"]
--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,7 @@
-$redis = Redis::Namespace.new("base_app", redis: Redis.new)
+if Rails.env.production?
+  redis = Redis.new(url: ENV['REDISTOGO_URL'])
+else
+  redis = Redis.new
+end
+
+$redis = Redis::Namespace.new("base_app", redis: redis)
