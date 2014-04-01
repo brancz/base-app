02e3186264b06d94e4aae2d8c835b1632a15c92e
flower-pot <flowerpot@pondati.net>
2014-04-01 14:09:17 +0200

    add template tests

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -2,8 +2,6 @@ class TemplatesController < ApplicationController
   layout false
   helper_method :devise_mapping
 
-  before_filter lambda{ puts "Logged in? -> #{user_signed_in?}" }
-
   def template
     render template: 'templates/' + params[:path], layout: false
   end
--- spec/controllers/templates_controller_spec.rb
+++ spec/controllers/templates_controller_spec.rb
@@ -2,4 +2,53 @@ require 'spec_helper'
 
 describe TemplatesController do
 
+  describe "GET 'index'" do
+    it "returns http success" do
+      get 'template', {path: 'index'}
+      response.should be_success
+    end
+  end
+
+  describe "GET 'users/confirm'" do
+    it "returns http success" do
+      get 'template', {path: 'users/confirm'}
+      response.should be_success
+    end
+  end
+
+  describe "GET 'users/profile'" do
+    it "returns http success" do
+      get 'template', {path: 'users/profile'}
+      response.should be_success
+    end
+  end
+
+  describe "GET 'users/sign_up'" do
+    it "returns http success" do
+      get 'template', {path: 'users/sign_up'}
+      response.should be_success
+    end
+  end
+
+  describe "GET 'users/forgot_password'" do
+    it "returns http success" do
+      get 'template', {path: 'users/forgot_password'}
+      response.should be_success
+    end
+  end
+
+  describe "GET 'users/sign_in'" do
+    it "returns http success" do
+      get 'template', {path: 'users/sign_in'}
+      response.should be_success
+    end
+  end
+
+  describe "GET 'users/unlock'" do
+    it "returns http success" do
+      get 'template', {path: 'users/unlock'}
+      response.should be_success
+    end
+  end
+
 end
