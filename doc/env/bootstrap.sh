2b4958e60fa4fe34fce7dd1e0f3970f057b8d8ef
flower-pot <flower-pot@flowerpot.io>
2014-04-05 18:23:49 +0200

    remove nodejs from bootstrap
    it is not needed, therubyracer is sufficient for precompiling assets

--- doc/env/bootstrap.sh
+++ doc/env/bootstrap.sh
@@ -4,13 +4,6 @@ sudo apt-get update -q -y
 
 sudo apt-get -y -q install gawk libgdbm-dev pkg-config libffi-dev build-essential openssl libreadline6 libreadline6-dev curl git zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev autoconf libc6-dev libncurses5-dev automake libtool bison subversion python postgresql postgresql-contrib libpq-dev redis-server python-software-properties
 
-sudo apt-add-repository -y ppa:chris-lea/node.js
-sudo apt-get update -q -y
-sudo apt-get install nodejs -q -y
-sudo apt-get install npm -q -y
-sudo npm install -g karma
-sudo ln -s /usr/bin/nodejs /usr/bin/node &>/dev/null
-
 su -l vagrant -c "curl -L https://get.rvm.io | bash -s stable --rails"
 su -l vagrant -c "source \"/home/vagrant/.rvm/scripts/rvm\""
 
