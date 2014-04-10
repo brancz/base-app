13e0d8d70bb90fc1863d93f39e57ce6e628b8e23
flower-pot <flower-pot@flowerpot.io>
2014-04-10 22:52:40 +0200

    fix role tests

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -3,12 +3,12 @@ require 'spec_helper'
 describe Role do
   it "has a number of users" do
     role = Role.find_by_internal_name("user")
-    role.users_count.should == 0
+    role.users_count.should == 1
   end
 
   it "has one user assiciated if it is created" do
     role = Role.find_by_internal_name("user")
     user = FactoryGirl.create(:user)
-    role.users_count.should == 1 # user role is default
+    role.users_count.should == 2 # user role is default from user created above + default created admin
   end
 end
