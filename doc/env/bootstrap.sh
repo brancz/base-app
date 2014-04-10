47198bdb782f5a64f9ed6348af9ed711dc98d0dc
flower-pot <flowerpot@pondati.net>
2014-04-10 12:27:09 +0200

    downgrade ruby to 2.1.0
    there are precompiled ruby binaries available for 2.1.0 on ubuntu 12.04
      -> no compilation needed

--- doc/env/bootstrap.sh
+++ doc/env/bootstrap.sh
@@ -4,7 +4,9 @@ sudo apt-get update -q -y
 
 sudo apt-get -y -q install gawk libgdbm-dev pkg-config libffi-dev build-essential openssl libreadline6 libreadline6-dev curl git zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev autoconf libc6-dev libncurses5-dev automake libtool bison subversion python postgresql postgresql-contrib libpq-dev redis-server python-software-properties
 
-su -l vagrant -c "curl -L https://get.rvm.io | bash -s stable --rails"
+su -l vagrant -c "curl -L https://get.rvm.io | bash -s stable"
 su -l vagrant -c "source \"/home/vagrant/.rvm/scripts/rvm\""
+su -l vagrant -c "rvm install ruby-2.1.0"
+su -l vagrant -c "gem install bundler"
 
 sudo -u postgres psql -U postgres -d postgres -c "CREATE USER base_app WITH PASSWORD 'password1' CREATEDB;"
