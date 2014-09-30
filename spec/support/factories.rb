ea2e05a968b1605720e031745b1da384d0001431
flower-pot <flower-pot@flowerpot.io>
2014-09-30 21:38:10 +0200

    fix factory

--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :user, aliases: [:owner] do
     sequence(:email) {|n| "user#{n}@example.com" }
+    sequence(:username) {|n| "user#{n}" }
     password "secretpassword"
     confirmation_sent_at = Time.now
     after(:create) { |user| user.confirm! }
