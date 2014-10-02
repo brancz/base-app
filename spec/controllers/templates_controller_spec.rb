835be17b321a34881cd6ef2a25a396c061235d7d
flower-pot <flower-pot@flowerpot.io>
2014-10-02 18:39:16 +0200

    fix test

--- spec/controllers/templates_controller_spec.rb
+++ spec/controllers/templates_controller_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe TemplatesController do
   describe "GET 'index'" do
     it "returns http success" do
-      get 'template', {path: 'index'}
+      get 'template', {path: 'home'}
       response.should be_success
     end
   end
